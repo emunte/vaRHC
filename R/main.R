4254ab3739878b74eaaeec74fc78775bddcfea5c
Elisabet Munté <49304354+emunte@users.noreply.github.com>
2022-10-18 16:06:27 +0200

    Add files via upload

--- R/main.R
+++ R/main.R
@@ -5,7 +5,7 @@
 NULL
 
 #' vaR()
-#' @description  vaR function firstly collects different type of information related to the variant of interest (e.g. Population frequencies, in-silico predictions, some functional studies...). 
+#' @description  vaR function firstly collects different type of information related to the variant of interest (e.g. Population frequencies, in-silico predictions, some functional studies...).
 #' Secondly,  it mixes all this information and following modified ACMG rules it returns the final classification of the variant and it specifies which criteria are given. Optionally, all the information can be printed in an excel file.
 #' @param assembly character hg19 or hg38
 #' @param gene gene of interest
@@ -20,7 +20,7 @@ NULL
 #' @spliceai.annotation Path to gene annotation file. By default it uses the file stored in docs folder: "../docs/gencode_spliceai_hg19.txt"
 #' @spliceai.distance  Integer. Maximum distance between the variant and gained/lost splice site (default: 1000)
 #' @spliceai.masked Mask scores representing annotated acceptor/donor gain and unannotated acceptor/donor loss (default: 1)
-#' @provean.program Logical. By default is FALSE and it is assumed that provean program is not installed in your computer. 
+#' @provean.program Logical. By default is FALSE and it is assumed that provean program is not installed in your computer.
 #' @param excel.results Logical. By default is FALSE and no excel file would be produced. If TRUE and excel file will be saved
 #' @param path.original.file If excel.results param is set to TRUE, path.original.file must contain the path to the excel template. By default is the template located in the package docs folder.
 #' @param path.copy.file By default is NULL. If excel.results param is set to TRUE, path.copy.file must provide the path where the excel file must be saved. If not provided it will be saved in the working directory.
@@ -28,7 +28,7 @@ NULL
 #' @examples
 #' vaR (assembly= "hg19", gene= "BRCA1", variant= "c.211A>G",  excel.results=TRUE, path.copy.file="./excels/")
 #' vaR.all <- vaR (assembly = "hg19", gene = "BRCA1", variant = "c.692C>T", spliceai.program = TRUE, spliceai.reference = "../docs/hg19.fa", excel.results = TRUE, path.copy.file="../excels" )
-#' @references 
+#' @references
 #' Richards, S., Aziz, N., Bale, S., Bick, D., Das, S., Gastier-Foster, J., Grody, W. W., Hegde, M., Lyon, E., Spector, E., Voelkerding, K., Rehm, H. L., & ACMG Laboratory Quality Assurance Committee (2015). Standards and guidelines for the interpretation of sequence variants: a joint consensus recommendation of the American College of Medical Genetics and Genomics and the Association for Molecular Pathology. Genetics in medicine : official journal of the American College of Medical Genetics, 17(5), 405–424. https://doi.org/10.1038/gim.2015.30
 #' Tavtigian, S. V., Harrison, S. M., Boucher, K. M., & Biesecker, L. G. (2020). Fitting a naturally scaled point system to the ACMG/AMP variant classification guidelines. Human mutation, 41(10), 1734–1737. https://doi.org/10.1002/humu.24088
 #' Garrett, A., Durkie, M., Callaway, A., Burghel, G. J., Robinson, R., Drummond, J., Torr, B., Cubuk, C., Berry, I. R., Wallace, A. J., Ellard, S., Eccles, D. M., Tischkowitz, M., Hanson, H., Turnbull, C., & CanVIG-UK (2021). Combining evidence for and against pathogenicity for variants in cancer susceptibility genes: CanVIG-UK consensus recommendations. Journal of medical genetics, 58(5), 297–304. https://doi.org/10.1136/jmedgenet-2020-107248
@@ -36,6 +36,7 @@ NULL
 #' ClinGen PTEN Expert Panel Specifications to the ACMG/AMP Variant Interpretation Guidelines Version 2: https://clinicalgenome.org/site/assets/files/4000/clingen_pten_acmg_specifications_v2.pdf
 #' ClinGen InSiGHT Hereditary Colorectal Cancer/Polyposis Variant Curation Expert Panel Specifications to the ACMG/AMP Variant Interpretation Guidelines Version 1 (draft): https://www.insight-group.org/content/uploads/2021/11/DRAFT_Nov_2021_TEMPLATE_SVI.ACMG_Specifications_InSiGHT_MMR_V1.pdf
 #' Feliubadaló, L., Moles-Fernández, A., Santamariña-Pena, M., Sánchez, A. T., López-Novo, A., Porras, L. M., Blanco, A., Capellá, G., de la Hoya, M., Molina, I. J., Osorio, A., Pineda, M., Rueda, D., de la Cruz, X., Diez, O., Ruiz-Ponte, C., Gutiérrez-Enríquez, S., Vega, A., & Lázaro, C. (2021). A Collaborative Effort to Define Classification Criteria for ATM Variants in Hereditary Cancer Patients. Clinical chemistry, 67(3), 518–533. https://doi.org/10.1093/clinchem/hvaa250
+#'@export
 vaR <- function(assembly, gene, variant, NM=NULL, NC = NULL, CCDS=NULL, gene.specific.df=NULL, browser="firefox", spliceai.program = FALSE, spliceai.reference = NULL, spliceai.annotation = system.file("docs", "gencode_spliceai_hg19.txt", package="vaRHC"), spliceai.distance = 1000, spliceai.masked = 1, provean.program = FALSE, excel.results = FALSE,  path.copy.file = NULL ){
   cat("looking for VariantInfo, please wait\n")
   info <- vaRinfo(assembly,  gene, variant, NM = NM, NC = NC, CCDS = CCDS, gene.specific.df = gene.specific.df, browser= browser, spliceai.program = spliceai.program, spliceai.reference = spliceai.reference, spliceai.annotation= spliceai.annotation, spliceai.distance= spliceai.distance, spliceai.masked = spliceai.masked, provean.program = provean.program)
@@ -45,7 +46,7 @@ vaR <- function(assembly, gene, variant, NM=NULL, NC = NULL, CCDS=NULL, gene.spe
     cat("printing information in an excel file,  please wait \n")
     vaRreport(info, class, path.copy.file)
   }
-  return(list(vaRinfo = info, 
+  return(list(vaRinfo = info,
               vaRclass = class))
 }
 
@@ -66,7 +67,7 @@ vaR <- function(assembly, gene, variant, NM=NULL, NC = NULL, CCDS=NULL, gene.spe
 #' @spliceai.annotation Path to gene annotation file. By default it uses the file stored in docs folder: "../docs/gencode_spliceai_hg19.txt"
 #' @spliceai.distance  Integer. Maximum distance between the variant and gained/lost splice site (default: 1000)
 #' @spliceai.masked Mask scores representing annotated acceptor/donor gain and unannotated acceptor/donor loss (default: 1)
-#' @provean.program Logical. By default is FALSE and it is assumed that provean program is not installed in your computer. 
+#' @provean.program Logical. By default is FALSE and it is assumed that provean program is not installed in your computer.
 #' @param excel.results Logical. By default is FALSE and no excel file would be produced. If TRUE and excel file will be saved
 #' @param path.copy.file By default is NULL. If excel.results param is set to TRUE, path.copy.file must provide the path where the excel file must be saved. If not provided it will be saved in the working directory.
 #' @author Elisabet Munté Roca
@@ -75,18 +76,18 @@ vaR <- function(assembly, gene, variant, NM=NULL, NC = NULL, CCDS=NULL, gene.spe
 # all <- vaRbatch(all.variants, spliceai.program = FALSE, excel.results = TRUE, path.original.file="../docs/template.xlsx", path.copy.file="/media/emunte/Nuevo/Variants_diagnostic/NS49/excels" )
 #' @export
 vaRbatch <- function (assembly = "hg19", all.variants, gene.specific.df=NULL, browser="firefox", spliceai.program = FALSE, spliceai.reference = NULL, spliceai.annotation = system.file("docs", "gencode.v38lift37.annotation.txt", package="vaRHC"), spliceai.distance = 1000, spliceai.masked = 1, provean.program = FALSE, print.data.frame = TRUE, excel.results = FALSE, path.copy.file = NULL){
-  time <-  Sys.time() %>% 
+  time <-  Sys.time() %>%
     stringr::str_replace_all("-|:| ", "_")
   log.file <- file.path(getwd(), "log")
   dir.create(log.file, showWarnings = FALSE)
   file.create(paste0(log.file,"/", time, ".log"))
   all.variants.list <- list()
-  df.variants2 <- data.frame(NM=NULL, gene=NULL, variant=NULL, prot=NULL, final_class=NULL, all_criteria=NULL, 
-                             PVS1=NULL, PS1=NULL,  PS3=NULL, 
-                             PM1=NULL, PM2=NULL, PM4=NULL, PM5=NULL,  PP3=NULL,  
-                             BA1=NULL, BS1=NULL, BS2=NULL, BS3=NULL, BP4=NULL,BP7=NULL, 
-                             clinvar_class=NULL, clinvar_review=NULL, 
-                             spliceAI_AG_score=NULL, spliceAI_AG_dis=NULL,spliceAI_AL_score=NULL, spliceAI_AL_dis=NULL,spliceAI_DG_score=NULL, spliceAI_DG_dis=NULL, spliceAI_DL_score=NULL, spliceAI_DL_dis=NULL, 
+  df.variants2 <- data.frame(NM=NULL, gene=NULL, variant=NULL, prot=NULL, final_class=NULL, all_criteria=NULL,
+                             PVS1=NULL, PS1=NULL,  PS3=NULL,
+                             PM1=NULL, PM2=NULL, PM4=NULL, PM5=NULL,  PP3=NULL,
+                             BA1=NULL, BS1=NULL, BS2=NULL, BS3=NULL, BP4=NULL,BP7=NULL,
+                             clinvar_class=NULL, clinvar_review=NULL,
+                             spliceAI_AG_score=NULL, spliceAI_AG_dis=NULL,spliceAI_AL_score=NULL, spliceAI_AL_dis=NULL,spliceAI_DG_score=NULL, spliceAI_DG_dis=NULL, spliceAI_DL_score=NULL, spliceAI_DL_dis=NULL,
                              revel=NULL )
   for (i in 1:nrow(all.variants)){
     cat(paste(i, "/", nrow(all.variants), "variants \n"))
@@ -97,38 +98,38 @@ vaRbatch <- function (assembly = "hg19", all.variants, gene.specific.df=NULL, br
     CCDS <- NULL
     tryCatch({
       time.start <- Sys.time()
-      info.R <- vaR(assembly = assembly, 
-                    gene = gene, 
-                    variant = variant, 
-                    NM = NM, 
-                    NC = NC, 
-                    CCDS = CCDS,  
-                    gene.specific.df = gene.specific.df,  
-                    browser = browser, 
-                    spliceai.program = spliceai.program, 
-                    spliceai.reference = spliceai.reference, 
-                    spliceai.annotation = spliceai.annotation, 
-                    spliceai.distance = spliceai.distance, 
-                    spliceai.masked = spliceai.masked, 
-                    provean.program = provean.program,  
-                    excel.results = excel.results, 
+      info.R <- vaR(assembly = assembly,
+                    gene = gene,
+                    variant = variant,
+                    NM = NM,
+                    NC = NC,
+                    CCDS = CCDS,
+                    gene.specific.df = gene.specific.df,
+                    browser = browser,
+                    spliceai.program = spliceai.program,
+                    spliceai.reference = spliceai.reference,
+                    spliceai.annotation = spliceai.annotation,
+                    spliceai.distance = spliceai.distance,
+                    spliceai.masked = spliceai.masked,
+                    provean.program = provean.program,
+                    excel.results = excel.results,
                     path.copy.file= path.copy.file)
       crit <- paste0(info.R$vaRclass$final.classification$criteria.assigned  , collapse=";")
       if(print.data.frame == TRUE){
-        df.variants2 <- rbind(df.variants2, 
+        df.variants2 <- rbind(df.variants2,
                               c(info.R$vaRinfo$Variant.Info$NM,
                                 info.R$vaRinfo$Variant.Info$gene,
-                                info.R$vaRinfo$Variant.Info$variant, 
+                                info.R$vaRinfo$Variant.Info$variant,
                                 info.R$vaRinfo$Variant.Info$protein,
                                 info.R$vaRclass$final.classification$final.class,
-                                crit, 
+                                crit,
                                 info.R$vaRclass$final.criteria$PVS1.message,
-                                info.R$vaRclass$final.criteria$PS1.message, 
-                                info.R$vaRclass$final.criteria$PS3.message, 
+                                info.R$vaRclass$final.criteria$PS1.message,
+                                info.R$vaRclass$final.criteria$PS3.message,
                                 info.R$vaRclass$final.criteria$PM1.message,
                                 info.R$vaRclass$final.criteria$PM2.message,
                                 info.R$vaRclass$final.criteria$PM4.message,
-                                info.R$vaRclass$final.criteria$PM5.message, 
+                                info.R$vaRclass$final.criteria$PM5.message,
                                 info.R$vaRclass$final.criteria$PP3.message,
                                 info.R$vaRclass$final.criteria$BA1.message,
                                 info.R$vaRclass$final.criteria$BS1.message,
@@ -148,39 +149,39 @@ vaRbatch <- function (assembly = "hg19", all.variants, gene.specific.df=NULL, br
                                 info.R$vaRinfo$predictors$predictor.table[17,"position"],
                                 info.R$vaRinfo$predictors$predictor.table[4,"values"]
                               ))
-        names(df.variants2) <- c("NM", "gene", "variant", "prot", "final_class", "all_criteria", 
-                                 "PVS1", "PS1", "PS3",  
-                                 "PM1", "PM2",  "PM4", "PM5",  "PP3", 
-                                 "BA1", "BS1", "BS2", "BS3", "BP4", "BP7", 
-                                 "clinvar_class", "clinvar_review", 
-                                 "spliceAI_AG_score", "spliceAI_AG_dis","spliceAI_AL_score", "spliceAI_AL_dis", "spliceAI_DG_score", "spliceAI_DG_dis",  "spliceAI_DL_score", "spliceAI_DL_dis", 
+        names(df.variants2) <- c("NM", "gene", "variant", "prot", "final_class", "all_criteria",
+                                 "PVS1", "PS1", "PS3",
+                                 "PM1", "PM2",  "PM4", "PM5",  "PP3",
+                                 "BA1", "BS1", "BS2", "BS3", "BP4", "BP7",
+                                 "clinvar_class", "clinvar_review",
+                                 "spliceAI_AG_score", "spliceAI_AG_dis","spliceAI_AL_score", "spliceAI_AL_dis", "spliceAI_DG_score", "spliceAI_DG_dis",  "spliceAI_DL_score", "spliceAI_DL_dis",
                                  "REVEL")
         output <- file.path(getwd(), "output")
         dir.create(output, showWarnings = FALSE)
-        write.table(x = df.variants2, 
-                    file = file.path(output, paste0(time, "variants.txt")), 
+        write.table(x = df.variants2,
+                    file = file.path(output, paste0(time, "variants.txt")),
                     row.names = F)
       }
-      
+
       time.end <- Sys.time()
       time.diff <- time.end - time.start
-      write.table(x = paste( gene, variant, "executed in ", time.diff), 
-                  file = file.path(log.file, paste0(time, ".log")), 
-                  append=TRUE, 
-                  col.names = FALSE, 
+      write.table(x = paste( gene, variant, "executed in ", time.diff),
+                  file = file.path(log.file, paste0(time, ".log")),
+                  append=TRUE,
+                  col.names = FALSE,
                   row.names = FALSE)
       name <-  paste0(gene,"_", variant)
       all.variants.list[[name]] <-  info.R
-      
+
     }, error = function(e){
-      write.table(x = paste(gene, variant, "ERROR:", conditionMessage(e)), 
-                  file = file.path(log.file, paste0(time, ".log")), 
-                  append=TRUE, 
-                  col.names = FALSE, 
+      write.table(x = paste(gene, variant, "ERROR:", conditionMessage(e)),
+                  file = file.path(log.file, paste0(time, ".log")),
+                  append=TRUE,
+                  col.names = FALSE,
                   row.names = FALSE)
     })
   }
   cat (paste0("Find log file: ",  file.path(log.file, paste0(time, ".log"))))
   return(info.R)
-} 
+}
 
